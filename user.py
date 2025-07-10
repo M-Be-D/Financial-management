1e26e38e16ad853c6520967aec2b83aa68d4cd67
M-Be-D <site.sabt.me@gmail.com>
2025-07-10 11:10:42 +0330

    add description and comment

--- user.py
+++ user.py
@@ -1,20 +1,28 @@
+# libraries
 import os
 import json
 
+# class 'User' for add new users
 class User:
+    """
+    # Config users
+    * To add new user use 'create_user' method
+    * To login use 'login' method
+    * To submit financial information use 'save_financial_data' method
+    """
     def __init__(self, users_list:str):
         self.default_value = {
             "role": "user",
             "password": None,
             "income": [],
             "expense": {
-                "title": [],
-                "amount": [],
-                "category": [],
-                "description": [],
-                "average": 0
+                "title": [], # عنوان
+                "amount": [], # مبلغ
+                "category": [], # دسته بندی
+                "description": [], # توضیحات
+                "average": 0 
             },
-            "saving": 0
+            "saving": 0 # پس‌انداز
         }
         self.users_path = users_list
         with open(users_list, "r") as u:
@@ -22,6 +30,9 @@ class User:
         self.usernames = self.users.keys()
 
     def create_user(self):
+        """
+        To add new user
+        """
         username = input("Please enter username: ")
         password = input("Enter password: ")
         
@@ -40,6 +51,9 @@ class User:
                 u.close()
         
     def login(self, username, password):
+        """
+        To login
+        """
         if username in self.usernames:
             if self.users[username]["password"] == password:
                 return True
@@ -51,12 +65,16 @@ class User:
             return False
         
     def save_financial_data(self, username):
-        
+        """
+        To submit financial information
+        """
+        # submit income
         def add_income(income):
             self.users[username]["income"].append(income)
             saving = sum(self.users[username]["income"]) - sum(self.users[username]["expanse"]["amount"])
             self.users[username]["saving"] = saving
 
+        # submit expense
         def add_expense(title, amount, category, description):
             self.users[username]["expanse"]["title"].append(title)
             self.users[username]["expanse"]["amount"].append(amount)
