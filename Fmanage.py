3bcbec6fb83289e9982807313176192c9b0b6b36
M-Be-D <site.sabt.me@gmail.com>
2025-07-11 17:58:47 +0330

    Using baners randomly

--- Fmanage.py
+++ Fmanage.py
@@ -1,3 +1,11 @@
 # start
 from user import User
 from admin import Admin
+import banner
+import random
+
+# banner
+banners = [banner.banner1, banner.banner2, banner.banner3]
+select_banner = random.choice(banners)
+select_banner()
+banner.connect()
